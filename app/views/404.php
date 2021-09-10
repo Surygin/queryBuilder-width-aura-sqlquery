b7ffc01423b010899b15565c6e20107dc866ebac
Антон Сурыгин <surygin@live.com>
2021-09-10 13:41:21 +0300

    Подключение league templates

---
+++ app/views/404.php
@@ -0,0 +1,10 @@
+<?php $this->layout('template', ['title' => 'About']) ?>
+
+<div class="container">
+    <div class="row">
+        <h1>This is About page!</h1>
+        <p>Hello, <?=$this->e($name)?></p>
+    </div>
+    <!-- /.row -->
+</div>
+<!-- /.container -->
